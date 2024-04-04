ccff991971d41fbaae020765b5283ab33b8c1595
Andrew Mark Feferman <57198537+mfeferman-cx@users.noreply.github.com>
2024-04-04 11:49:59 -0500

    Update SanityScanApplication.java
    Updated the class definition to extend SpringBootServletInitializer.

--- src/main/java/com/sanity/scan/SanityScanApplication.java
+++ src/main/java/com/sanity/scan/SanityScanApplication.java
@@ -2,12 +2,18 @@ package com.sanity.scan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SanityScanApplication {
+public class SanityScanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SanityScanApplication.class, args);
 	}
 
+	@Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        	return builder.sources(StartWebApplication.class);
+    	}
 }
